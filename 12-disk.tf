bcbbf94bf5c801c2d63d883a01afbba8b7ac34b7
Glend Maatita <glend.maatita@gmail.com>
2024-03-20 14:08:31 +0700

    create dis

---
+++ 12-disk.tf
@@ -0,0 +1,8 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_disk
+resource "google_compute_disk" "onxp-exercise-storage" {
+  name  = "onxp-exercise-storage"
+  zone  = "${var.region}-a"
+
+  size = 10
+  physical_block_size_bytes = 4096
+}
