135c3c7221a994074d7c426314a8496df4ac6f9d
Glend Maatita <glend.maatita@gmail.com>
2024-03-29 21:23:39 +0700

    cleanup

--- argocd-repository.tf
+++ argocd-repository.tf
@@ -2,7 +2,7 @@ resource "argocd_repository" "csp-k8s-exercise" {
   repo = "git@github.com:onxpnet/k8s-exercise.git"
   username = "git"
   name = "k8s-exercise"
-  ssh_private_key = base64decode("<base 64 ssh keys")
+  ssh_private_key = base64decode("<base 64 ssh keys>")
   insecure = true
 }
 
